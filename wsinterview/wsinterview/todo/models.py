437cfa258c716a8ede08673e1b7a7d36f0b096eb
Jonah Kim <jonah@honeybook.com>
2019-08-13 11:54:41 +0300

    Initial migrations

---
+++ wsinterview/wsinterview/todo/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class CalendarEvent(models.Model):
+    """
+    A calenar event
+    """
+
+    datetime = models.DateTimeField()
+
+
+class Task(models.Model):
+    """
+    A task in the TODO list
+    """
+
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    due_date = models.ForeignKey(CalendarEvent, on_delete=models.CASCADE)
+
+
+class TaskLabel(models.Model):
+    """
+    Label for task
+    """
+
+    name = models.CharField(max_length=100)
+    tasks = models.ManyToManyField(Task)
